72622f423340710b7fcdc544eb6b58e80d534e21
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-05-26 20:04:43 +0300

    Initial commit

---
+++ build/CMakeFiles/deadline_reminder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deadline_reminder.dir/main.cpp.o"
+  "CMakeFiles/deadline_reminder.dir/main.cpp.o.d"
+  "deadline_reminder"
+  "deadline_reminder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deadline_reminder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
